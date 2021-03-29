b71f9dc2b8501867a8215e506df8fd2e3492c2e4
Potapova Sofia <ptv-sofia@yandex.by>
2021-03-29 23:39:52 +0300

    init basic.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    let newArr = []
+    let isDiscard = false
+    for (let i = 0; i < arr.length; i++) {
+      if (!isDiscard) {
+        if (arr[i] == '--double-next' && i !== arr.length - 1) {
+          newArr.push(arr[i + 1])
+        } else if (arr[i] == '--double-prev' && i !== 0 && arr[i - 2] !== '--discard-next') {
+          newArr.push(arr[i - 1])
+        } else if (arr[i] == '--discard-prev' && arr[i - 2] !== '--discard-next') {
+          newArr.pop()
+        } else if (arr[i] == '--discard-next') {
+          isDiscard = true
+        } else if (arr[i] !== '--double-prev' && arr[i] !== '--double-next' && arr[i] !== '--discard-prev') {
+          newArr.push(arr[i])
+        }
+      } else isDiscard = false
+    }
+    return newArr
+  } else throw Error
 };
